0190659c16fd94ab7c00add3629e489e1de7fe9d
Ken <none>
2017-06-24 08:00:58 -0400

    Added summer code.

---
+++ summer/week1/area_circle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+
+def get_area_circle(r):
+    """Return a float for area of a circle of radius, r."""
+    return -1.0
---
+++ summer/week1/area_rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+
+def get_area_rectangle(w, h):
+    """Return a float for area of a rectangle that has width, w, and height, h."""
+    return -1.0
