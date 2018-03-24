e0f253ae2e71c9f029698861401d48c21af5d2a5
BBonMACLIN <balune.boy@gmail.com>
2018-03-24 10:17:23 -0400

    Clean up this branch.

--- summer/week1/area_circle.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-
-def get_area_circle(r):
-    """Return a float for area of a circle of radius, r."""
-    return -1.0
--- summer/week1/area_rectangle.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-
-def get_area_rectangle(w, h):
-    """Return a float for area of a rectangle that has width, w, and height, h."""
-    return -1.0
