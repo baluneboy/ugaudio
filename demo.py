4a4e61cca0a05057287fd160539546ecc547cdf9
BBonMACLIN <balune.boy@gmail.com>
2018-03-24 10:11:02 -0400

    Branch for shuttle data (like USMP-4, STS-87).

--- demo.py
+++ demo.py
@@ -1,141 +1,102 @@
 #!/usr/bin/env python
 
-import aifc
+import glob
 import os.path
 import numpy as np
 import matplotlib.pyplot as plt
-from ugaudio.load import padread
-from ugaudio.create import get_chirp
-from ugaudio.signal import normalize
-from ugaudio.pad import PadFile
 
-def demo_chirp(fs=44100):
-    """simple demo of chirp signal (not representative of ug accel data)"""
 
-    print '\ndemo with artificial chirp signal (not representative of ug accel data)'
+def get_accel_dir(day, unit, head, usmp4_dir='/home/ken/data/usmp4'):
+    """return accel subdirectory string given integer day and strings for unit, head and usmp4 dir"""
+    # like /home/ken/data/usmp4/usmp_4F_1/HEADB/DAY000/ACCEL
+    u = 'usmp_4%s_1' % unit
+    h = 'HEAD%s' % head
+    d = 'DAY%03d' % day
+    return os.path.join(usmp4_dir, u, h, d, 'ACCEL')
 
-    # get signal of interest
-    y = get_chirp()
 
-    # demean signal
-    data = y - y.mean(axis=0)
+def get_hour_files(axis, day, hour, unit, head, usmp4_dir='/home/ken/data/usmp4'):
+    """return list of accel data files for given axis, day, hour, etc."""
+    acc_dir = get_accel_dir(day, unit=unit, head=head, usmp4_dir=usmp4_dir)
+    glob_pat = os.path.join(acc_dir, '%s%sM%03d%02d.*' % (head, axis, day, hour))  # full filename to like BXM00018.55R
+    files = sorted(glob.glob(glob_pat))
+    return files
 
-    # normalize to range -32768:32767 (actually, use -32000:32000)
-    data = data * 32000.0
-    
-    # data conditioning
-    data = data.astype(np.int16) # not sure why we need this...maybe aifc assumptions
-    data = data.byteswap().newbyteorder() # need this on mac osx and linux (windows?)
-    
-    # convert data to string for aifc to work write
-    strdata = data.tostring()
-    aiff_file = 'demo_chirp.aiff'
-    g = aifc.open(aiff_file, 'w')
-    sampwidth = 2
-    #nchannels, sampwidth, framerate, nframes, comptype, compname
-    g.setparams((1, sampwidth, fs, len(data), 'NONE', 'not compressed'))
-    g.writeframes(strdata)
-    g.close()
-    print 'wrote demo chirp sound file %s' % aiff_file
-    
-    # plot data
-    png_file = 'demo_chirp.png'   
-    plt.plot(data)
-    plt.savefig(png_file)    
-    print 'wrote demo accel plot file  %s' % png_file
 
-def demo_accel():
-    """somewhat representative of microgravity accel data from ISS"""
-
-    print '\ndemo with actual acceleration data from GMT 17-Oct-2014'
-
-    # get PAD file from example dir
-    _DIR = os.path.dirname(__file__)
-    filename = os.path.join(_DIR, 'examples/2014_10_17_06_31_15.515+2014_10_17_06_41_15.528.121f02')
-    
-    if not os.path.exists(filename):
-        print 'BUT COULD NOT FIND\n%s\nso abort demo\n' % filename
-        raise SystemExit
-    
-    # get PadFile and attempt conversion
-    pad_file = PadFile(filename)
-    
-    try:
-        pad_file.convert(plot=True)
-        msg = 'wrote PNG & AIFF files for...\n%s\nsee your "examples" directory' % pad_file
-    
-    except:
-        msg = 'could not convert...\n%s\nis everything okay with "examples" directory?' % pad_file
-    
-    print msg + '\n'
-
-def demo_accel_file(data_file, axis='x'):
-    """demo arbitrary file with microgravity accel data from ISS"""
-
-    print '\ndemo with actual acceleration data from %s (if file exists)' % data_file
-    
-    # get PadFile and attempt conversion
-    pad_file = PadFile(data_file)
-    
-    if not pad_file.exists:
-       raise Exception('data file "%s" does not exist', data_file)
-       
-    try:
-        pad_file.convert(plot=True, axis=axis)
-        msg = 'wrote PNG & AIFF files for %s-axis of %s' % (axis, pad_file)
-    
-    except:
-        msg = 'could not convert to audio (or plot) %s' % pad_file
-    
-    print msg + '\n'
-
-def show_samplerate(header_file):
-    """return sample rate (samples/sec) for input header file"""
-    pad_file = PadFile(header_file)
-    print pad_file
-
-def demo_build_numpy_array(sensor, y, m, d):
-    import glob
-    import datetime
-    import numpy as np
+def build_numpy_array(fnames):
+    """build numpy array from data read from list of filenames"""
     from ugaudio.load import padread
-    from pims.utils.pimsdateutil import datetime_to_ymd_path
-    ymd_dir = datetime_to_ymd_path(datetime.date(y, m, d))
-    glob_pat = '%s/*_accel_%s/*%s' % (ymd_dir, sensor, sensor)
-    fnames = glob.glob(glob_pat)
-    fnames = ['/tmp/rogue_pad_file.sensor']
-    arr = np.empty((0, 5), dtype=np.float32)    # float32 matches what we read from PAD file
+    arr = np.empty((0, 1), dtype=np.float32)
+    print 'building array...'
     for fname in fnames:
-        # read data from file (not using double type here like MATLAB would, so we get courser demeaning)
-        a = padread(fname)
-        a[:,1:4] = a[:,1:4] - a[:,1:4].mean(axis=0)  # demean x, y and z columns
-        v = np.array( np.sqrt(a[:,1]**2 + a[:,2]**2 + a[:,3]**2) )  # compute vector magnitude
-        print v
-        #new_col = np.reshape(v, (-1, 1))
-        ncols = 1
-        v.shape = (v.size//ncols, ncols)
-        #print v.shape, a.shape
-        a = np.append(a, v, axis=1) # append to get 5th column for vecmag
-        #print v.shape, a.shape
+        # read data from file
+        a = padread(fname, columns=1)
+        a[:, 0] = a[:, 0] - a[:, 0].mean(axis=0)  # demean column(s)
+        print '+ %d data pts from %s' % (a.shape[0], fname)
         arr = np.append(arr, a, axis=0)
-        #print arr.shape        
-    return arr    
-    
+    return arr[:, 0]
+
+
+def get_sleep_to_wake_files(usmp4_dir):
+    """get list of accel data files that show sleep to wake transition"""
+    axis, day, unit, head = 'X', 0, 'F', 'B'
+    some_files = []
+    for hr in [18, 19]:
+        some_files += get_hour_files(axis, day, hr, unit, head, usmp4_dir=usmp4_dir)
+    return some_files
+
+
+def plot_sleep_to_wake_data():
+
+    # example to load sleep-to-wake transition data
+    usmp4_dir = '/home/ken/data/usmp4'
+    xfiles = get_sleep_to_wake_files(usmp4_dir)
+    x = build_numpy_array(xfiles)
+    print '%d total data pts in array' % x.shape[0]
+
+    # plot color spectrogram to see crew wake
+    NFFT = 4096  # the length of the windowing segments (finer freq. resolution comes from larger NFFT)
+    Fs = 125.0  # samples per second
+
+    # build time array (seconds relative to first file start)
+    dt = 1.0 / Fs
+    t = np.arange(0.0, x.shape[0] / Fs, dt)
+
+    # figure for plots
+    fig = plt.figure(num=None, figsize=(8, 6), dpi=120, facecolor='w', edgecolor='k')
+
+    # plot x-axis accel. time domain data
+    ax1 = plt.subplot(211)
+    plt.plot(t, x)
+    plt.ylabel('X-Axis Accel. [g]')
+
+    # plot x-axis spectrogram frequency domain data (to better see crew wake transition)
+    # Pxx is the segments x freqs array of instantaneous power, freqs is
+    # the frequency vector, bins are the centers of the time bins in which
+    # the power is computed, and im is the matplotlib.image.AxesImage
+    # instance
+    ax2 = plt.subplot(212, sharex=ax1)
+    Pxx, freqs, bins, im = plt.specgram(x[:], NFFT=NFFT, Fs=Fs, noverlap=NFFT/2, cmap='jet', vmin=-140, vmax=-70)
+
+    # set y-limits for cut-off frequency of this sensor head (25 Hz)
+    plt.ylim((0, 25))
+
+    # add labeling
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Relative Time [sec]')
+
+    # add colorbar
+    fig.colorbar(im)
+
+    # fix ax position
+    pos1, pos2 = ax1.get_position(), ax2.get_position()  # get the original positions
+    pos1 = [pos1.x0, pos1.y0, pos2.width, pos1.height]
+    ax1.set_position(pos1)  # set a new position
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    
-    ## get sample rate from header file
-    #header_file = '/misc/yoda/pub/pad/year2017/month04/day01/sams2_accel_121f04/2017_04_01_20_55_05.415+2017_04_01_21_05_05.426.121f04.header'
-    #data_file = header_file.replace('.header', '')
-    #show_samplerate(data_file)
-    
-    ## generate artficial chirp, then create sound file (AIFF format) and plot it (PNG format)
-    #demo_chirp()
-    
-    # plot SAMS TSH (es06) data file (just one axis)
-    data_file = '/tmp/2017_05_22_23_39_02.803+2017_05_22_23_49_02.861.es06'
-    #show_samplerate(data_file)
-    demo_accel_file(data_file, axis='x') # just x-axis here
-    
-    ## build numpy array (appending rows for each PAD file and appending vecmag column along the way)
-    #arr = demo_build_numpy_array('121f03006', 2017, 11, 1)
-    #print arr
+    # Compare plot to Appendix page B-7 of Summary Report of Mission Acceleration Measurements for STS-87
+    plot_sleep_to_wake_data()
+
